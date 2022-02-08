faa6e3acec22dc7ce9466a6ab63dcf2edc0e9bd6
Piotr_Marzec <piotrmarzec77@gmail.com>
2022-02-08 17:45:31 +0100

    Dodanie metody w klasie Quiz dotyczacej gracza

--- src/Quiz.java
+++ src/Quiz.java
@@ -16,17 +16,16 @@ public class Quiz {
         scanner = new Scanner(System.in);
         currentPlayer = new Score();
     }
-
     public void start() {
 
 
-        // Utworzenie nowego wyniku dla aktualnego gracza
-        currentPlayer = new Score();
-        askForPlayerName();
-        runQuiz();
+            // Utworzenie nowego wyniku dla aktualnego gracza
+            currentPlayer = new Score();
+            askForPlayerName();
+            runQuiz();
+            askForNewGame();
 
-
-    }
+        }
 
 
     private void askForPlayerName() {
@@ -35,7 +34,6 @@ public class Quiz {
         String name = scanner.nextLine();
         currentPlayer.setPlayerName(name);
     }
-
     private void runQuiz() {
         System.out.println("Przed Toba " + questions.size() + " pytan. Powodzenia!");
         char userAnswer;
@@ -45,7 +43,7 @@ public class Quiz {
         Collections.shuffle(questions, new Random());
 
         // Kazde pytanie z listy jest wyswietlane i pobierana jest odpowiedz gracza
-        for (Question question : questions) {
+        for(Question question : questions) {
             question.print(questionNumber);
             System.out.println("Podaj odpowedz (A/B/C/D):");
             userAnswer = scanner.nextLine().charAt(0);
@@ -57,6 +55,35 @@ public class Quiz {
             questionNumber++;
         }
 
+    }
 
+    private void askForNewGame() {
+        // Wyswietlenie zapytania o ponowna gre oraz pobranie odpowiedzi
+        // Jesli odpowiedz jest twierdzaca, to quiz zaczyna sie od poczatku
+        // Jesli jest przeczaca, to wyniki sa zapisywane do pliku, a program sie konczy
+        // W przypadku blednej wartosci funkcja wywolywana jest ponownie
+        int choice;
+
+        System.out.println("Czy chcesz zagrac ponownie?");
+        System.out.println("1. Tak");
+        System.out.println("2. Nie");
+        System.out.println("Podaj wybor (1 lub 2):");
+
+        try {
+            choice = Integer.parseInt(scanner.nextLine());
+            if(choice != 1 && choice != 2) {
+                throw new Exception();
+            }
+            if(choice == 1) {
+                start();
+            } else {
+                System.out.println("Dziekujemy za gre. Program zostanie wylaczony.");
+
+                System.exit(0);
+            }
+        } catch (Exception e) {
+            System.out.println("Podany wybor jest nieprawidlowy!");
+            askForNewGame();
+        }
     }
 }
