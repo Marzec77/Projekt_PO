9e27114e3e2d58b54ded27882f77a49adffe3eac
Piotr_Marzec <piotrmarzec77@gmail.com>
2022-02-08 17:30:59 +0100

    Implementacja obiektu score

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,15 +1,17 @@
 
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Quiz {
     private Scanner scanner;
-
+    private Score currentPlayer;
+    private List<Score> scores;
 
     Quiz() {
 
         scanner = new Scanner(System.in);
-
+        currentPlayer = new Score();
     }
 
 
