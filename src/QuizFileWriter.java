026db085a370de329e67bb0f32920ad64ff50f49
Piotr_Marzec <piotrmarzec77@gmail.com>
2022-02-08 17:57:33 +0100

    Obsługa klasy QuizFileWriter

--- src/QuizFileWriter.java
+++ src/QuizFileWriter.java
@@ -1,2 +1,35 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class QuizFileWriter {
+    private final String SCORES_FILE_PATH;
+
+    QuizFileWriter(String scoresFilePath) {
+        this.SCORES_FILE_PATH = scoresFilePath;
+    }
+
+    public void saveScoresToFile(List<Score> scores) {
+        try {
+            FileWriter fileWriter = new FileWriter(SCORES_FILE_PATH);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            String fileContent = "";
+            // Dane kazdego wyniku sa dodawane jako nowa linia do ciagu znakow
+            // Przykladowo: Jan;10
+            for(Score score : scores) {
+                fileContent += score.getPlayerName() + ";" + score.getPoints() + "\n";
+            }
+
+            // Utworzony z wpisow o graczach ciag jest zapisywany do plikus
+            bufferedWriter.write(fileContent);
+            bufferedWriter.close();
+        }
+        catch (IOException e) {
+            // W przypadku bledu wyswietlany jest komunikat
+            System.out.println("Nie udalo sie zapisac wynikow do pliku!");
+        }
+    }
 }
